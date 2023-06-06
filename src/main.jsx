2c665efd60f818f9e7930da26848add8d5265c6a
Petizo Yiyo <petizo999@gmail.com>
2023-06-06 16:46:03 -0400

    se agrega hashrouter y basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,68 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, createBrowserRouter} from 'react-router-dom';
+import { BrowserRouter, HashRouter, createBrowserRouter} from 'react-router-dom';
 import { AppDespensa } from './AppDespensa';
 
 import './assets/colorBootstrap.css';
 import './assets/styles.css';
 import './assets/stylesCalendario.css';
 import './assets/correcionesBootstrap.css';
-/*import { InicioPage } from './pages/InicioPage';
-import { BodegasPage } from './pages/BodegasPage';
-import { AdquisicionesPage } from './pages/AdquisicionesPage';
-import { DespachosPage } from './pages/DespachosPage';
-import { StockPage } from './pages/StockPage';
-import { PlantillaArticuloPage } from './pages/PlantillaArticuloPage';
-import { InstanciaArticuloPage } from './pages/InstanciaArticuloPage';
-import { LoginPage } from './pages/LoginPage';¨/
 
-const subDominio = (location.pathname === '/Despensa-react/' ) ? '/Despensa-react': '';
-console.log('location.pathname',location.pathname);
 
-/*createBrowserRouter( [
-            {
-                path: 'login',
-                element: <LoginPage />,
-            },
-            {
-                path: '/',
-                element: <InicioPage />,
-            },
-            {
-                path: 'bodegas',
-                element: <BodegasPage />,
-            },
-            {
-                path: 'adquisiciones',
-                element: <AdquisicionesPage />,
-            },
-            {
-                path: 'despachos',
-                element: <DespachosPage />,
-            },
-            {
-                path: 'stock',
-                element: <StockPage />,
-            },
-            {
-                path: 'articulo-plantilla',
-                element: <PlantillaArticuloPage />,
-            },
-            {
-                path: 'articulo-instancia',
-                element: <InstanciaArticuloPage />,
-            }
-  ], {
-    basename: subDominio,
-  });
-*/
+const subDominio = (location.pathname === '/Despensa-react/' ) ? location.pathname: '';
+//console.log('location.pathname',location.pathname);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     
     <React.StrictMode>
-        <BrowserRouter basename={'/Despensa-react'}>
+        <HashRouter basename={subDominio}>
             <AppDespensa />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
    
     
